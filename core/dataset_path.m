1b332a320bd8bc7dad0d3cf9e87735063ba194da
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2023-04-11 12:26:56 -0600

    Create dataset_path.m

---
+++ core/dataset_path.m
@@ -0,0 +1,12 @@
+function full_path = dataset_path(filename)
+% DATASET_PATH Reads the conf file and returns the patht to the requested
+% dataset.
+%
+
+	% Load configuration file
+	conf = load_gconf(fullfile("..", "chilly.gconf"));
+	
+	% Append filename
+	full_path = fullfile(conf.data_dir, filename);
+
+end
