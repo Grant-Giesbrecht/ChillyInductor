f0606e035ddef49f6af8fe252a40502107a7d47f
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2023-05-26 15:07:19 -0600

    Added best guess of problem

--- Python Sim/Nonlinear_Inductor_Sim.py
+++ Python Sim/Nonlinear_Inductor_Sim.py
@@ -15,6 +15,15 @@ L0 = 30e-9
 
 # lks.crunch(.01, .03, show_plot_td=True, show_plot_spec=True)
 
+print(f"{Fore.RED}The problem is that the FFT on Iac acts as if Iac is I vs t, when it's really")
+print(f"{Fore.RED}the amplitude multiplying a sine term. I think I need to add an extra sine in")
+print(f"{Fore.RED}there or similar. But Iac_td is sitting at 2.something and FFT is showing Iac")
+print(f"{Fore.RED} = 0, which agrees with the guess of Iac=0 (which is really setting the osc.")
+print(f"{Fore.RED}amplitude of Iac).")
+
+
+
+
 Ibias = [.01, .02, .03]
 
 lks = LKSystem(Pgen, C_, l_phys, freq, q, L0)
