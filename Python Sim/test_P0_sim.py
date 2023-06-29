33c4dea67b71e05f7309915e5448472f8a6dec2a
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2023-06-29 15:18:30 -0600

    Changed bias values for P0 test
    Ran into convergence issues, this is now an apples to apples comparison with the p0 branch.

--- Python Sim/test_P0_sim.py
+++ Python Sim/test_P0_sim.py
@@ -1,5 +1,12 @@
 from core import *
 
+################ These are the original values used in P0-branch #############
+
+# Get target values from MATLAB
+Idc_A = np.array([-0.03325, -0.030875, -0.0285, -0.026125, -0.02375, -0.021375, -0.019, -0.016625, -0.01425, -0.011875, -0.0095, -0.007125, -0.00475, -0.002375, 0, 0.002375, 0.00475, 0.007125, 0.0095, 0.011875, 0.01425, 0.016625, 0.019, 0.021375, 0.02375, 0.026125, 0.0285, 0.030875, 0.03325])
+Ifund_mA = np.array([1.5131, 1.6246, 1.4932, 1.6711, 1.7109, 1.817, 1.7177, 1.6502, 1.6852, 1.7652, 1.8488, 1.9048, 1.9497, 1.9705, 1.9756, 1.9717, 1.9531, 1.9116, 1.8525, 1.7779, 1.6928, 1.6577, 1.7247, 1.8184, 1.7306, 1.6805, 1.5131, 1.6378, 1.5375])
+Ifund_A = Ifund_mA/1e3
+
 ############################### CONFIGURE SYSTEM PARAMETERS ##################
 Pgen = 0 # dBm
 C_ = 121e-12
@@ -8,9 +15,9 @@ freq = 10e9
 q = .19
 L0 = 1e-6
 
-Ibias = np.linspace(1, 60, 61)*1e-3
+# Ibias = np.linspace(1, 60, 61)*1e-3
 
-Idc_A = np.array([-0.03325, -0.030875, -0.0285, -0.026125, -0.02375, -0.021375, -0.019, -0.016625, -0.01425, -0.011875, -0.0095, -0.007125, -0.00475, -0.002375, 0, 0.002375, 0.00475, 0.007125, 0.0095, 0.011875, 0.01425, 0.016625, 0.019, 0.021375, 0.02375, 0.026125, 0.0285, 0.030875, 0.03325])
+# Idc_A = np.array([-0.03325, -0.030875, -0.0285, -0.026125, -0.02375, -0.021375, -0.019, -0.016625, -0.01425, -0.011875, -0.0095, -0.007125, -0.00475, -0.002375, 0, 0.002375, 0.00475, 0.007125, 0.0095, 0.011875, 0.01425, 0.016625, 0.019, 0.021375, 0.02375, 0.026125, 0.0285, 0.030875, 0.03325])
 
 # Read file if no data provided
 with open("cryostat_sparams.pkl", 'rb') as fh:
@@ -26,14 +33,14 @@ lks.setopt('start_guess_method', GUESS_USE_LAST)
 lks.setopt('use_S21_loss', False)
 lks.select_simulator(SIMULATOR_P0)
 
-lks.solve(Ibias)
+lks.solve(Idc_A)
 
 Iac = np.array([x.Iac for x in lks.sim_p0.solution])
 
 
 
 plt.figure(1)
-plt.plot(Ibias*1e3, Iac*1e3, color='b', linestyle='dashed', marker='o')
+plt.plot(Idc_A*1e3, Iac*1e3, color='b', linestyle='dashed', marker='o')
 plt.grid()
 plt.xlabel("Bias Current (mA)")
 plt.ylabel("AC Current Amplitude (mA)")
