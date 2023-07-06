69c3c9ed38eae87d1006d7c57a9cb75751065290
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2023-07-06 09:58:51 -0600

    Resolved conflict

--- scripts/DS31_TAE31_check.m
+++ scripts/DS31_TAE31_check.m
@@ -3,8 +3,9 @@
 thetaA_deg = 20;
 thetaB_deg = 30;
 
-thetaA = thetaA_deg*pi/180;
-thetaB = thetaB_deg*pi/180;
+thetaA = thetaA_deg.*pi./180;
+thetaB = thetaB_deg.*pi./180;
+Vg = 10;
 
 Zg = 50;
 Z0 = 40;
@@ -28,21 +29,20 @@ Ix = IL.*ZL.*j./Z0.*sin(thetaB) + IL.*cos(thetaB);
 
 Ig = Vx.*j./Z0.*sin(thetaA) + Ix.*cos(thetaA);
 
-VL = IL*ZL;
-
 %% Solve with P0-method
 
 Zin = xfmr2zin(Z0, ZL, thetaA+thetaB);
 Zin_x = xfmr2zin(Z0, ZL, thetaB);
 
-P0 = 1./2.*Vg.^2.*real(Zin)./( (real(Zin) + real(Zg)).^2 + (imag(Zin) + imag(Zg)).^2 ); % From Pozar
+P0 = 1./2.*Vg.^2.*real(Zin)./( (real(Zin) + real(Zg)).^2 + (imag(Zin) + imag(Zg)).^2 ); % From Pozar_4e, page 77, eq. 2,76
 
 thetaX = angle(Zin_x);
-Ix_P0based = sqrt(2.*P0./cos(thetaX)./Zin_x);
-Vx_P0based = sqrt( 2.*P0.*Zin_x./cos(thetaX) );
-IL_P0based = sqrt(2.*P0./cos(0)./ZL );
+Ix_P0based = sqrt( 2.*P0./cos(thetaX)./Zin_x );
+Vx_P0based = sqrt(2.*P0.*Zin_x./cos(thetaX));
+IL_P0based = sqrt( 2.*P0./cos(0)./ZL );
 
-%% PRint reuslts
+
+%% Print reuslts
 
 VI_theta = abs(angle(Vx)-angle(Ix));
 Px = 1/2.*abs(Vx).*abs(Ix).*cos(VI_theta);
@@ -55,19 +55,25 @@ displ("Ix: ", abs(Ix), " A @ ", 180./pi.*angle(Ix), " deg");
 displ();
 displ("Px = ", Px, " W");
 
-displ();
-displ("arg(Ix) - arg(IL): ", 180/pi*(angle(Ix)-angle(IL)) ," deg");
-displ("arg(Vx) - arg(Ix): ", 180/pi*(angle(Vx)-angle(Ix)) ," deg");
+displ()
+displ("arg(Ix) - arg(IL): ", 180./pi.*(angle(Ix)-angle(IL)), " deg");
+displ("arg(Vx) - arg(Ix): ", 180./pi.*(angle(Vx)-angle(Ix)), " deg");
 
+displ();
 barprint("P0 Based");
+
 displ("IL: ", abs(IL_P0based), " A @ ", 180./pi.*angle(IL_P0based), " deg");
 displ("Vx: ", abs(Vx_P0based), " V @ ", 180./pi.*angle(Vx_P0based), " deg");
 displ("Ix: ", abs(Ix_P0based), " A @ ", 180./pi.*angle(Ix_P0based), " deg");
 
-displ();
-displ("Px = ", Px, " W");
+displ()
+displ("P0 = ", P0, " W");
 
-displ();
-displ("arg(Ix) - arg(IL): ", 180/pi*(angle(Ix_P0based)-angle(IL_P0based)) ," deg");
-displ("arg(Vx) - arg(Ix): ", 180/pi*(angle(Vx_P0based)-angle(Ix_P0based)) ," deg");
+displ()
+displ("arg(Ix) - arg(IL): ", 180./pi.*(angle(Ix_P0based)-angle(IL_P0based)), " deg");
+displ("arg(Vx) - arg(Ix): ", 180./pi.*(angle(Vx_P0based)-angle(Ix_P0based)), " deg");
+
+
+
+
 
