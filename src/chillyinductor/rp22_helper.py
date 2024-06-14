e346fc88169405f13bcbe9b3c9f05e8fb019eaae
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-06-14 12:58:41 -0600

    Moved functions to ganymede

--- src/chillyinductor/rp22_helper.py
+++ src/chillyinductor/rp22_helper.py
@@ -570,82 +570,7 @@ def calc_sa_conditions(sa_conf, f_rf:float, f_lo:float, print_error:bool=False, 
 		return None
 
 
-def write_level(fh:h5py.File, level_data:dict):
-	''' Writes a dictionary to the hdf file.
-	 
-	Recursive function used by  '''
-	
-	# Scan over each directory of root-data
-	for k, v in level_data.items():
-		
-		# If value is a dictionary, this key represents a directory
-		if type(v) == dict:
-			
-			# Create a new group
-			fh.create_group(k)
-			
-			# Write the dictionary to the group
-			write_level(fh[k], v)
-				
-		else: # Otherwise try to write this datatype (ex. list of floats)
-			
-			# Write value as a dataset
-			fh.create_dataset(k, data=v)
 
-def save_hdf(root_data:dict, save_file:str, use_json_backup:bool=True) -> bool:
-	''' Writes a dictionary to an HDF file per the rules used by 'write_level()'. 
-	
-	* If the value of a key in another dictionary, the key is made a group (directory).
-	* If the value of the key is anything other than a dictionary, it assumes
-	  it can be saved to HDF (such as a list of floats), and saves it as a dataset (variable).
-	
-	'''
-	
-	# Start timer
-	t0 = time.time()
-	
-	# Open HDF
-	hdf_successful = True
-	exception_str = ""
-	
-	# Recursively write HDF file
-	with h5py.File(save_file, 'w') as fh:
-		
-		# Try to write dictionary
-		try:
-			write_level(fh, root_data)
-		except Exception as e:
-			hdf_successful = False
-			exception_str = f"{e}"
-	
-	# Check success condition
-	if hdf_successful:
-		print(f"Wrote file in {time.time()-t0} sec.")
-		
-		return True
-	else:
-		print(f"Failed to write HDF file! ({exception_str})")
-		
-		# Write JSON as a backup if requested
-		if use_json_backup:
-			
-			# Add JSON extension
-			save_file_json = save_file[:-3]+".json"
-			
-			# Open and save JSON file
-			try:
-				with open(save_file_json, "w") as outfile:
-					outfile.write(json.dumps(root_data, indent=4))
-			except Exception as e:
-				print(f"Failed to write JSON backup: ({e}).")
-				return False
-		
-		return True
-
-def hdf5_to_dict(filename) -> bool:
-	''' Reads a HDF file and converts the data to a dictionary '''
-	
-	for k in keys
 
 def dict_to_hdf5(json_data:dict, save_file) -> bool:
 	''' Converts a dict from MS1-style datasets to an HDF5 file.
@@ -907,11 +832,6 @@ def bin_empirical(data:np.ndarray, jump_size:int=None, jump_scale:int=None, show
 	
 	
 	return data_out
-	
-	
-	
-	
-	
 	
 	
 
