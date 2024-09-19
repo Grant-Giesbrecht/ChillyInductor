0b60a02fe1be8a52258ffbbd83262aba29ac4379
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-09-19 10:05:40 -0600

    AS3-MS2 updated w shaded error bars

--- RP-22 Scripts/SMC-B/Analysis Scripts/AS3-MS2_critical_temp_equilib.py
+++ RP-22 Scripts/SMC-B/Analysis Scripts/AS3-MS2_critical_temp_equilib.py
@@ -16,6 +16,56 @@ import pickle
 #------------------------------------------------------------
 # Import Data
 
+def rounding_bin(x:list, y:list, step:float=0.2, tol:float=0.05):
+
+	def round_x(val:float, rounding_thresh:float):
+		return round(val/rounding_thresh)*rounding_thresh
+	
+	# xmin = np.min(x)
+	# xmax = np.max(x)
+	
+	# # Get list of values to round to
+	# rounding_points = np.arrange(0, np.ceil(xmax/step)*step, step=step)
+	
+	x_out = []
+	y_out = []
+	
+	used_x_bins = []
+	bin_ys = []
+	y_stds = []
+	y_means = []
+	
+	# Scan over x list
+	for yv, xv in zip(y, x):
+		
+		# Find closest rounding point
+		rp = round_x(xv, step)
+		delta = np.abs(rp - xv)
+		
+		# If delta is less than tolerance, add point
+		if delta <= tol:
+			x_out.append(rp)
+			y_out.append(yv)
+			
+			# Add to std lists
+			if rp in used_x_bins:
+				
+				idx = used_x_bins.index(rp)
+				bin_ys[idx].append(yv)
+			else:
+				used_x_bins.append(rp)
+				bin_ys.append([yv])
+	
+	# Calculate standard deviations
+	for xv, ylists in zip(used_x_bins, bin_ys):
+		y_means.append(np.mean(ylists))
+		y_stds.append(np.std(ylists))
+		
+		print(f"X: {xv}, len(y)={len(ylists)}, std={np.std(ylists)}, mean={np.mean(ylists)}, min={np.min(ylists)}, max={np.max(ylists)}")
+	
+	return (x_out, y_out, used_x_bins, y_means, y_stds)
+			
+
 datapath = get_datadir_path(rp=22, smc='B', sub_dirs=['*R3C4*B', 'Track 1 4mm'])
 datapath = get_datadir_path(rp=22, smc='B', sub_dirs=['*R4C4*C', 'Track 1 4mm'])
 # datapath = "C:\\Users\\Grant Giesbrecht\\Documents\\GitHub\\ChillyInductor\\RP-22 Scripts\\SMC-B\\Measurement Scripts\\data"
@@ -99,6 +149,44 @@ ax1_2.set_xlabel("Temperature (K)")
 ax1_2.set_title("Resistance over Temperature")
 # ax1_2.set_xlim([2.8, 4.4])
 plt.grid(True)
+
+xytup = rounding_bin(x=temperatures, y=(resistances-sc_res), step=0.2, tol=0.05)
+temp_rd = xytup[0]
+res_rd = np.array(xytup[1])
+temp_bins = np.array(xytup[2])
+res_means = np.array(xytup[3])
+res_std = np.array(xytup[4])
+
+fig3 = plt.figure(3)
+ax1_3 = fig3.gca()
+ax1_3.scatter(temp_rd, res_rd, marker='.', color=(0.7, 0, 0))
+ax1_3.errorbar(temp_bins, res_means, yerr=res_std, color=(0.2, 0.2, 0.6), elinewidth=0.5)
+ax1_3.fill_between(temp_bins, res_means - 2*res_std, res_means + 2*res_std, color=(0.2, 0.2, 0.6), alpha=0.2)
+ax1_3.fill_between(temp_bins, res_means - res_std, res_means + res_std, color=(0.2, 0.2, 0.6), alpha=0.4)
+
+ax1_3.set_ylabel("Resistance (Ohms)")
+ax1_3.set_xlabel("Binned Temperature (K)")
+ax1_3.set_title("Resistance over Temperature")
+# ax1_2.set_xlim([2.8, 4.4])
+plt.grid(True)
+
+fig4 = plt.figure(4)
+ax1_4 = fig4.gca()
+ax1_4.scatter(temp_rd, res_rd/1e3, marker='.', color=(0.7, 0, 0))
+ax1_4.fill_between(temp_bins, res_means/1e3 - 3*res_std/1e3, res_means/1e3 + 3*res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.075, linewidth=0)
+ax1_4.fill_between(temp_bins, res_means/1e3 - 2*res_std/1e3, res_means/1e3 + 2*res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.075, linewidth=0)
+ax1_4.fill_between(temp_bins, res_means/1e3 - 1.75*res_std/1e3, res_means/1e3 + 1.75*res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.075, linewidth=0)
+ax1_4.fill_between(temp_bins, res_means/1e3 - 1.5*res_std/1e3, res_means/1e3 + 1.5*res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.075, linewidth=0)
+ax1_4.fill_between(temp_bins, res_means/1e3 - 1.25*res_std/1e3, res_means/1e3 + 1.25*res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.075, linewidth=0)
+ax1_4.fill_between(temp_bins, res_means/1e3 - res_std/1e3, res_means/1e3 + res_std/1e3, color=(0.2, 0.2, 0.6), alpha=0.3)
+
+ax1_4.set_ylabel("Resistance (kOhms)")
+ax1_4.set_xlabel("Measured Temperature (K)")
+ax1_4.set_title("Critical Tempertature Measurement")
+# ax1_2.set_xlim([2.8, 4.4])
+plt.grid(True)
+
+
 
 # Save pickled-figs
 pickle.dump(fig1, open(os.path.join("..", "Figures", f"AS3-fig1-{filename}.pklfig"), 'wb'))
