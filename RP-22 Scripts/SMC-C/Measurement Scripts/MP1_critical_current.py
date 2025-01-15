c2f1f176406d32d47fadf033a0403a9821b00dcb
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-01-15 14:14:51 -0700

    Updated MP1

--- RP-22 Scripts/SMC-C/Measurement Scripts/MP1_critical_current.py
+++ RP-22 Scripts/SMC-C/Measurement Scripts/MP1_critical_current.py
@@ -41,9 +41,12 @@ THRESHOLD_NORMAL_V = 0.25
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--detail', help="Show detailed log messages.", action='store_true')
+parser.add_argument('-x', '--disablerf', help="Disable RF power in sweep.", action='store_true')
 parser.add_argument('--loglevel', help="Set the logging display level.", choices=['LOWDEBUG', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], type=str.upper)
 parser.add_argument('--tempwait', help="Set the logging display level.", type=float)
 args = parser.parse_args()
+
+disable_rf = args.disablerf
 
 # Initialize log
 log = LogPile()
@@ -78,7 +81,12 @@ try:
 	
 	idc_coarse_step_mA = float(conf_data['Idc_coarse_step_mA']) # Step used for step-1 climbing
 	idc_resolution_step_mA = float(conf_data['Idc_resolution_step_mA']) # Step used for step-2, zeroing-in
+	idc_fine_step_mA = float(conf_data['Idc_fine_step_mA']) # Step size used for step-3 fine-climbing
+	
 	norm_extra_Z = float(conf_data['extra_Z_norm_ohm'])
+	Ic_uncert_backoff_mult = float(conf_data['Ic_uncert_backoff_mult'])
+	
+	sa_conf = conf_data['spectrum_analyzer']
 except:
 	log.critical(f"Corrupt configuration file >{conf_file_name}<.")
 	exit()
@@ -117,13 +125,12 @@ else:
 	exit()
 sig_gen.set_enable_rf(False)
 
-# vna = RohdeSchwarzZVA("", log)
-# if vna.online:
-# 	log.info("R&S VNA >ONLINE<")
-# else:
-# 	log.critical("Failed to connect to R&S VNA!")
-# 	exit()
-# vna.set_enable_rf(False)
+spec_an = RohdeSchwarzFSE("GPIB::20::INSTR", log)
+if spec_an.online:
+	log.info("Rohde & Schwarz FSEK spectrum analyzer >ONLINE<")
+else:
+	log.critical("Failed to connect to Rohde & Schwarz FSEK spectrum analyzer!")
+	exit()
 
 ##======================================================
 # Get User Input re: sweep
@@ -141,6 +148,14 @@ mfli.set_output_ac_enable(False)
 mfli.set_differential_enable(True)
 mfli.set_offset(0)
 mfli.set_output_enable(True)
+
+##======================================================
+# Configure spectrum analyzer
+
+spec_an.set_ref_level(10)
+
+spec_an.set_continuous_trigger(False) # Otherwise wait-ready will not work!
+spec_an.set_y_div(20) # Set y scale so *everything* is visible
 
 ##======================================================
 # Calibrate current-sense resistor
@@ -164,8 +179,16 @@ try:
 except Exception as e:
 	self_contents = f"ERROR: Failed to read script source. ({e})"
 
-# # Define dataset dictionary
-dataset = {"calibration":{"current_sense_res_ohms": cs_res, "time_of_meas": str(cs_time)}, "dataset":{"freq_rf_GHz":[], "power_rf_dBm":[], "times":[], "MFLI_V_offset_V":[], "requested_Idc_mA":[], "raw_meas_Vdc_V":[], "Idc_mA":[], "detect_normal":[], "temperature_K":[]}, "summary_dataset": {'Ic_mA':[], "Ic_uncert_mA": []}, 'info': {'source_script': __file__, 'operator_notes':operator_notes, 'configuration': json.dumps(conf_data), 'source_script_full':self_contents}}
+
+# Define dataset dictionary
+calibration_dict = {"current_sense_res_ohms": cs_res, "time_of_meas": str(cs_time)}
+dataset_dict = {"freq_rf_GHz":[], "power_rf_dBm":[], "times":[], "MFLI_V_offset_V":[], "requested_Idc_mA":[], "raw_meas_Vdc_V":[], "Idc_mA":[], "detect_normal":[], "temperature_K":[], "waveform_f_Hz":[], "waveform_s_dBm":[], "waveform_rbw_Hz":[], "measurement_stage":[]}
+
+summary_dict = {'Ic_meas_mA':[], "Ic_set_mA":[], "Ic_uncert_mA": [], "power_rf_dBm":[], "freq_rf_GHz":[], "times":[], "temperature_K":[]}
+
+info_dict = {'source_script': __file__, 'operator_notes':operator_notes, 'configuration': json.dumps(conf_data), 'source_script_full':self_contents}
+
+dataset = {"calibration":calibration_dict, "dataset":dataset_dict, "summary_dataset":summary_dict, 'info':info_dict}
 
 
 # Prepare DMM to measure voltage
@@ -224,8 +247,9 @@ mfli.set_offset(0)
 ##======================================================
 # Begin logging data
 
-freq_rf = [1e9]
-power_rf_dBm = [-40]
+if disable_rf:
+	freq_rf = [1e9]
+	power_rf_dBm = [-40]
 
 # Scan over all frequencies
 abort = False
@@ -248,8 +272,20 @@ for f_rf in freq_rf:
 		
 		log.info(f"Searching for Ic: Ramping up until normal.")
 		
+		# Prepare the signal generator
+		if disable_rf:
+			sig_gen.set_enable_rf(False)
+		else:
+			sig_gen.set_freq(f_rf)
+			sig_gen.set_power(p_rf)
+			sig_gen.set_enable_rf(True)
+		
+		# Prepare the spectrum analyzer
+		sa_conditions = calc_sa_conditions(sa_conf, f_rf, f_lo=None)
+		
 		#------ Find critical current ----------
 		# Step 1: Move up in steps until you go normal
+		measurement_stage = 0
 		
 		highest_sc_meas = None
 		highest_sc_set = 0
@@ -279,6 +315,52 @@ for f_rf in freq_rf:
 			# Read temperature
 			t_meas = tempctrl.get_temp()
 			
+			# Set conditions for spectrum analyzer
+			wav_rbw = None
+			wav_f = None
+			wav_s = None
+			for idx_sac, sac in enumerate(sa_conditions):
+				
+				fstart = sac['f_start']
+				fend = sac['f_end']
+				frbw = sac['rbw']
+				
+				log.debug(f"Spectrum analyzer measuring frequency range >{fstart/1e6}< MHz to >{fend/1e6}< MHz, RBW = >:q{frbw/1e3}< kHz.")
+				
+				# Configure spectrum analyzer
+				spec_an.set_res_bandwidth(frbw)
+				spec_an.set_freq_start(fstart)
+				spec_an.set_freq_end(fend)
+				
+				# Start trigger on spectrum analyzer
+				spec_an.send_manual_trigger()
+				
+				# Wait for FSQ to finish sweep
+				spec_an.wait_ready()
+				
+				# Get waveform
+				wvfrm = spec_an.get_trace_data(1)
+				
+				# Save data
+				rbw_list = [sac['rbw']]*len(wvfrm['x'])
+				if wav_f is None:
+					wav_f = wvfrm['x']
+					wav_s = wvfrm['y']
+					wav_rbw = rbw_list
+				else:
+					wav_f_temp = wav_f + wvfrm['x']
+					wav_s_temp = wav_s + wvfrm['y']
+					wav_rbw_temp = wav_rbw+ rbw_list
+					
+					# Sort result
+					wav_f = []
+					wav_s = []
+					wav_rbw = []
+					for wx,wy,wr in sorted(zip(wav_f_temp, wav_s_temp, wav_rbw_temp)):
+						wav_f.append(wx)
+						wav_s.append(wy)
+						wav_rbw.append(wr)
+			
 			# Calcualte additional resistance in system
 			system_Z = Voffset/I_meas_A
 			additional_Z = system_Z - current_set_res
@@ -300,7 +382,14 @@ for f_rf in freq_rf:
 			else:
 				highest_sc_meas = I_meas_A*1e3
 				highest_sc_set = idc_target
-		
+			
+			dataset['dataset']['freq_rf_GHz'].append(f_rf)
+			dataset['dataset']['power_rf_dBm'].append(p_rf)
+			
+			dataset['dataset']['waveform_f_Hz'].append(wav_f)
+			dataset['dataset']['waveform_s_dBm'].append(wav_s)
+			dataset['dataset']['waveform_rbw_Hz'].append(wav_rbw)
+			
 			dataset['dataset']['MFLI_V_offset_V'].append(Voffset)
 			dataset['dataset']['requested_Idc_mA'].append(idc_target)
 			dataset['dataset']['raw_meas_Vdc_V'].append(v_cs_read)
@@ -309,6 +398,7 @@ for f_rf in freq_rf:
 			
 			dataset['dataset']['temperature_K'].append(t_meas)
 			dataset['dataset']['times'].append(str(datetime.datetime.now()))
+			dataset['dataset']['measurement_stage'].append(measurement_stage)
 			
 			# Check for autosave
 			if time.time() - time_last_save > TIME_AUTOSAVE_S:
@@ -324,6 +414,7 @@ for f_rf in freq_rf:
 		
 		#------ Find critical current ----------
 		# Step 2: Perform binary search from max to locate critical current with tolerance
+		measurement_stage = 1
 		
 		log.info(f"Searching for Ic: Running binary search.")
 		
@@ -342,9 +433,6 @@ for f_rf in freq_rf:
 				
 				Ic = (est_norm_current + highest_sc_meas)/2
 				Ic_uncert = delta_meas
-				
-				dataset['summary_dataset']['Ic_mA'].append(Ic)
-				dataset['summary_dataset']['Ic_uncert_mA'].append(Ic_uncert)
 				
 				log.info(f"Ic = >{Ic}< mA +/- {Ic_uncert/2} mA")
 				
@@ -372,6 +460,52 @@ for f_rf in freq_rf:
 			# Read temperature
 			t_meas = tempctrl.get_temp()
 			
+			# Set conditions for spectrum analyzer
+			wav_rbw = None
+			wav_f = None
+			wav_s = None
+			for idx_sac, sac in enumerate(sa_conditions):
+				
+				fstart = sac['f_start']
+				fend = sac['f_end']
+				frbw = sac['rbw']
+				
+				log.debug(f"Spectrum analyzer measuring frequency range >{fstart/1e6}< MHz to >{fend/1e6}< MHz, RBW = >:q{frbw/1e3}< kHz.")
+				
+				# Configure spectrum analyzer
+				spec_an.set_res_bandwidth(frbw)
+				spec_an.set_freq_start(fstart)
+				spec_an.set_freq_end(fend)
+				
+				# Start trigger on spectrum analyzer
+				spec_an.send_manual_trigger()
+				
+				# Wait for FSQ to finish sweep
+				spec_an.wait_ready()
+				
+				# Get waveform
+				wvfrm = spec_an.get_trace_data(1)
+				
+				# Save data
+				rbw_list = [sac['rbw']]*len(wvfrm['x'])
+				if wav_f is None:
+					wav_f = wvfrm['x']
+					wav_s = wvfrm['y']
+					wav_rbw = rbw_list
+				else:
+					wav_f_temp = wav_f + wvfrm['x']
+					wav_s_temp = wav_s + wvfrm['y']
+					wav_rbw_temp = wav_rbw+ rbw_list
+					
+					# Sort result
+					wav_f = []
+					wav_s = []
+					wav_rbw = []
+					for wx,wy,wr in sorted(zip(wav_f_temp, wav_s_temp, wav_rbw_temp)):
+						wav_f.append(wx)
+						wav_s.append(wy)
+						wav_rbw.append(wr)
+			
 			# Calcualte additional resistance in system
 			system_Z = Voffset/I_meas_A
 			additional_Z = system_Z - current_set_res
@@ -394,13 +528,12 @@ for f_rf in freq_rf:
 			
 			log.debug(f"Saving datapoint.")
 			
-			# After all spectrum analyzer data has been captured, save all data to master dataset
-			# dataset['dataset']['freq_rf_GHz'].append(f_rf/1e9)
-			# dataset['dataset']['power_rf_dBm'].append(p_rf)
+			dataset['dataset']['freq_rf_GHz'].append(f_rf)
+			dataset['dataset']['power_rf_dBm'].append(p_rf)
 			
-			# dataset['dataset']['waveform_f_Hz'].append(wav_f)
-			# dataset['dataset']['waveform_s_dBm'].append(wav_s)
-			# dataset['dataset']['waveform_rbw_Hz'].append(wav_rbw)
+			dataset['dataset']['waveform_f_Hz'].append(wav_f)
+			dataset['dataset']['waveform_s_dBm'].append(wav_s)
+			dataset['dataset']['waveform_rbw_Hz'].append(wav_rbw)
 			
 			dataset['dataset']['MFLI_V_offset_V'].append(Voffset)
 			dataset['dataset']['requested_Idc_mA'].append(idc_target)
@@ -410,6 +543,157 @@ for f_rf in freq_rf:
 			
 			dataset['dataset']['temperature_K'].append(t_meas)
 			dataset['dataset']['times'].append(str(datetime.datetime.now()))
+			dataset['dataset']['measurement_stage'].append(measurement_stage)
+			
+			# Check for autosave
+			if time.time() - time_last_save > TIME_AUTOSAVE_S:
+				
+				# Save data and log
+				log.save_hdf(os.path.join(LOG_DIRECTORY, f"{sweep_name}_autosave.log.hdf"))
+				
+				dict_to_hdf(dataset, os.path.join(DATA_DIRECTORY, f"{sweep_name}_autosave.hdf"))
+				log.debug(f"Autosaved logs and data.")
+				
+				time_last_save = time.time()
+		
+		#------ Find critical current ----------
+		# Step 3: Fine stepped search
+		measurement_stage = 2
+		
+		log.info(f"Searching for Ic: Running fine stepped search.")
+		
+		Ic = (est_norm_current + highest_sc_meas)/2
+		Ic_uncert = delta_meas
+		
+		# Initialize target Idc from Ic and uncertainty.
+		idc_target = Ic - Ic_uncert*Ic_uncert_backoff_mult
+		last_sc_point_targ = None
+		last_sc_point_meas = None
+		
+		running = True
+		while running:
+			
+			# Increase target current
+			idc_target += idc_fine_step_mA
+			
+			# Set current, take measurements
+			
+			# Set offset voltage
+			Voffset = current_set_res * idc_target/1e3
+			log.debug(f"Selected offset voltage {Voffset} V from current_set_res={current_set_res} Ohms and idc={idc_target} mA")
+			mfli.set_offset(Voffset)
+			mfli.set_output_enable(True)
+			
+			# Read Vdc (-> Idc)
+			v_cs_read = np.abs(dmm.trigger_and_read())
+			
+			# Calculate current
+			I_meas_A = v_cs_read/cs_res
+			log.debug(f"Measured bias current at {I_meas_A*1e3} mA", detail=f"V_current_sense_read = {v_cs_read} V, CS-resistor = {cs_res} ohms.")
+			
+			# Read temperature
+			t_meas = tempctrl.get_temp()
+			
+			# Set conditions for spectrum analyzer
+			wav_rbw = None
+			wav_f = None
+			wav_s = None
+			for idx_sac, sac in enumerate(sa_conditions):
+				
+				fstart = sac['f_start']
+				fend = sac['f_end']
+				frbw = sac['rbw']
+				
+				log.debug(f"Spectrum analyzer measuring frequency range >{fstart/1e6}< MHz to >{fend/1e6}< MHz, RBW = >:q{frbw/1e3}< kHz.")
+				
+				# Configure spectrum analyzer
+				spec_an.set_res_bandwidth(frbw)
+				spec_an.set_freq_start(fstart)
+				spec_an.set_freq_end(fend)
+				
+				# Start trigger on spectrum analyzer
+				spec_an.send_manual_trigger()
+				
+				# Wait for FSQ to finish sweep
+				spec_an.wait_ready()
+				
+				# Get waveform
+				wvfrm = spec_an.get_trace_data(1)
+				
+				# Save data
+				rbw_list = [sac['rbw']]*len(wvfrm['x'])
+				if wav_f is None:
+					wav_f = wvfrm['x']
+					wav_s = wvfrm['y']
+					wav_rbw = rbw_list
+				else:
+					wav_f_temp = wav_f + wvfrm['x']
+					wav_s_temp = wav_s + wvfrm['y']
+					wav_rbw_temp = wav_rbw+ rbw_list
+					
+					# Sort result
+					wav_f = []
+					wav_s = []
+					wav_rbw = []
+					for wx,wy,wr in sorted(zip(wav_f_temp, wav_s_temp, wav_rbw_temp)):
+						wav_f.append(wx)
+						wav_s.append(wy)
+						wav_rbw.append(wr)
+			
+			# Calcualte additional resistance in system
+			system_Z = Voffset/I_meas_A
+			additional_Z = system_Z - current_set_res
+			if additional_Z > norm_extra_Z:
+				log.info(f"Chip detected as normal (>Z_extra = {additional_Z} Ohms<). Threshold is {norm_extra_Z} Ohms.")
+				
+				correction_factor = last_sc_point_meas/last_sc_point_targ
+				Ic_uncert_meas = correction_factor*idc_target - last_sc_point_meas
+				Ic_uncert_targ = idc_target - last_sc_point_targ
+				uncert_max = np.max([Ic_uncert_meas, Ic_uncert_targ])
+				
+				log.info(f"Saving summary data point: Ic_meas = >{last_sc_point_meas} mA< Ic_set = >{last_sc_point_targ} mA<, Ic_uncertainty = >:a{uncert_max} mA<.")
+				
+				# Save summary data
+				dataset['summary_dataset']['Ic_meas_mA'].append(last_sc_point_meas)
+				dataset['summary_dataset']['Ic_set_mA'].append(last_sc_point_targ)
+				dataset['summary_dataset']['Ic_uncert_mA'].append( uncert_max )
+				dataset['summary_dataset']['power_rf_dBm'].append(p_rf)
+				dataset['summary_dataset']['freq_rf_GHz'].append(f_rf)
+				dataset['summary_dataset']['times'].append(str(datetime.datetime.now()))
+				dataset['summary_dataset']['temperature_K'].append(t_meas)
+				
+				# Let chip recover
+				log.debug("Turning off DC bias and signal generator to let chip recover")
+				sig_gen.set_enable_rf(False)
+				mfli.set_offset(0)
+				log.debug(f"Waiting {RECOVERY_TIME_S} seconds for chip to recover.")
+				time.sleep(RECOVERY_TIME_S)
+				log.debug("Post-normal recovery procedure complete.")
+				
+				running = False
+			else:
+				log.debug(f"Chip remained superconducting.")
+				last_sc_point_targ = idc_target
+				last_sc_point_meas = I_meas_A*1e3
+			
+			log.debug(f"Saving datapoint.")
+			
+			dataset['dataset']['freq_rf_GHz'].append(f_rf)
+			dataset['dataset']['power_rf_dBm'].append(p_rf)
+			
+			dataset['dataset']['waveform_f_Hz'].append(wav_f)
+			dataset['dataset']['waveform_s_dBm'].append(wav_s)
+			dataset['dataset']['waveform_rbw_Hz'].append(wav_rbw)
+			
+			dataset['dataset']['MFLI_V_offset_V'].append(Voffset)
+			dataset['dataset']['requested_Idc_mA'].append(idc_target)
+			dataset['dataset']['raw_meas_Vdc_V'].append(v_cs_read)
+			dataset['dataset']['Idc_mA'].append(v_cs_read/cs_res*1e3)
+			dataset['dataset']['detect_normal'].append(norm_detected)
+			
+			dataset['dataset']['temperature_K'].append(t_meas)
+			dataset['dataset']['times'].append(str(datetime.datetime.now()))
+			dataset['dataset']['measurement_stage'].append(measurement_stage)
 			
 			# Check for autosave
 			if time.time() - time_last_save > TIME_AUTOSAVE_S:
