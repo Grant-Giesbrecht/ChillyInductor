c48646903907b0fd15ed258c5c4086b3057d04f0
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-07-16 12:15:47 -0600

    Updating analysis programs

--- RP-23 Scripts/SMC-C/Analysis Programs/AP2_f73_f81_chirp_fit.py
+++ RP-23 Scripts/SMC-C/Analysis Programs/AP2_f73_f81_chirp_fit.py
@@ -10,10 +10,8 @@ import sys
 import argparse
 import mplcursors
 from graf.base import sample_colormap
-from scipy.signal import hilbert
 import pylogfile.base as plf
-from scipy.optimize import curve_fit
-from scipy.interpolate import interp1d
+from rp23c_helper import *
 
 log = plf.LogPile()
 
@@ -21,152 +19,6 @@ path = "/Volumes/M6 T7S/ARC0 PhD Data/RP-23 Qubit Readout/Data/SMC-C July Campai
 
 trim_time = True
 time_bounds = [[-1300, -800]]
-
-def find_closest_index(lst, X):
-	closest_index = min(range(len(lst)), key=lambda i: abs(lst[i] - X))
-	return closest_index
-
-def find_zero_crossings(x, y):
-	''' Finds zero crossings of x, then uses y-data to interpolate between the points.'''
-	
-	signs = np.sign(y)
-	sign_changes = np.diff(signs)
-	zc_indices = np.where(sign_changes != 0)[0]
-	
-	# Trim end points - weird things can occur if the waveform starts or ends at zero
-	zc_indices = zc_indices[1:-1]
-	
-	# Interpolate each zero-crossing
-	cross_times = []
-	for zci in zc_indices:
-		dx = x[zci+1] - x[zci]
-		dy = y[zci+1] - y[zci]
-		frac = np.abs(y[zci]/dy)
-		cross_times.append(x[zci]+dx*frac)
-	
-	return cross_times
-
-class AP2Analyzer:
-	
-	def __init__(self, df:pd.DataFrame, power_dBm:float):
-		
-		self.time_ns = np.array(df['Time']*1e9)
-		self.wave_mV = np.array(df['Ampl']*1e3)
-		
-		if trim_time:
-			new_time_ns = []
-			new_wave_mV = []
-			
-			for prange in time_bounds:
-				idx_start = find_closest_index(self.time_ns, prange[0])
-				idx_end = find_closest_index(self.time_ns, prange[1])
-				new_time_ns = np.concatenate([new_time_ns, self.time_ns[idx_start:idx_end+1]])
-				new_wave_mV = np.concatenate([new_wave_mV, self.wave_mV[idx_start:idx_end+1]])
-			
-			self.time_ns = new_time_ns
-			self.wave_mV = new_wave_mV
-			
-			log.info(f"Trimmed time for dataset with P=>{power_dBm}< dBm.")
-		
-		# Calculate envelope
-		hilbert_temp = hilbert(self.wave_mV)
-		self.envelope = np.abs(hilbert_temp)
-		
-		self.calc_zero_cross()
-		
-		self.z_time_trim = []
-		self.z_freq_trim = []
-		self.fit_results = []
-		self.fit_curve = []
-		self.fit_data()
-		
-		self.power_dBm = power_dBm
-		
-		log.info(f"Analysis complete for dataset with P=>{power_dBm}< dBm.")
-	
-	def calc_zero_cross(self):
-		''' Calculate frequency using zero crossing analysis.'''
-		
-		tzc = find_zero_crossings(self.time_ns, self.wave_mV)
-		N_avg = 2
-		
-		# Select every other zero crossing to see full periods and become insensitive to y-offsets.
-		tzc_fullperiod = tzc[::2*N_avg]
-		periods = np.diff(tzc_fullperiod)
-		self.zcf_freq = (1/periods)*N_avg
-
-		self.zcf_time = tzc_fullperiod[:-1] + periods/2
-	
-	def fit_data(self):
-		''' Fit an SPM model to the experiment data. '''
-		
-		# Re-trim the data so that anything outside of an acceptable frequency range is removed.
-		f_max = 0.115
-		f_min = 0.085
-
-		# Find valid indices
-		valid_mask = (self.zcf_freq >= f_min) & (self.zcf_freq <= f_max)
-		if not np.any(valid_mask):
-			raise ValueError("No values found in wave_mV between wave_min and wave_max.")
-
-		start_index = np.argmax(valid_mask)  # first True index
-		end_index = len(valid_mask) - np.argmax(valid_mask[::-1])  # last True index
-
-		# Trim arrays
-		self.z_time_trim = self.zcf_time[start_index:end_index]*1e-9
-		self.z_freq_trim = self.zcf_freq[start_index:end_index]*1e9
-		
-		
-		omega_0 = 100e6
-		omega_tolerance = 5e6
-		
-		t_center = np.mean(self.z_time_trim)
-		t_center_tol = 35e-9
-		
-		
-		# --- Define the SPM-based model function ---
-		def spm_model(t, w0, t0, K1, K2, K3):
-			t_convert = (t-t0)
-			return w0 + (4 * np.pi * K1) / (K2 * K3**2) * (t_convert) * np.exp(-((t_convert)**2) / K3**2)
-		
-		# Interpolate amplitude_2 onto the time_ns grid
-		interp_func = interp1d(self.time_ns*1e-9, self.envelope, kind='linear', bounds_error=False, fill_value=0)
-		weights = interp_func(self.z_time_trim)
-		
-		# Avoid division by zero — add a small constant
-		weights += 1e-6
-		
-		# Convert amplitude weights to standard deviations (smaller sigma => higher weight)
-		sigma = 1.0 / weights
-		
-		# Set curve bounds
-		lower_bounds = [omega_0-omega_tolerance,  t_center-t_center_tol,    0.05e-5,    0.05,    10e-9]
-		# lower_bounds = [omega_0-omega_tolerance,  t_center-t_center_tol,    0.5e-5,    0.05,    30e-9]
-		# upper_bounds = [ omega_0+omega_tolerance, t_center+t_center_tol, 5e-5, 0.1, 70e-9]
-		upper_bounds = [ omega_0+omega_tolerance, t_center+t_center_tol, 5e-3, 0.1, 120e-9]
-		
-		# Set initial guess
-		initial_guess = [omega_0, t_center, 1.2e-5, 0.0625, 50e-9]
-		
-		# Fit the curve
-		popt, pcov = curve_fit(spm_model, self.z_time_trim, self.z_freq_trim, sigma=sigma, absolute_sigma=True, p0=initial_guess, bounds=(lower_bounds, upper_bounds))
-		
-		print(f"Sigma = {sigma}")
-		print(f"popt = {popt}")
-		
-		self.fit_results = popt
-		
-		# Plot results
-		self.fit_curve = spm_model(self.z_time_trim, *popt)
-		
-		# plt.plot(self.z_time_trim, self.z_freq_trim, label='Original Data', linestyle=':', marker='.', color=(0, 0.6, 0.6))
-		# plt.plot(self.z_time_trim, self.fit_curve, label='Fit', linestyle='--', linewidth=0.75, color=(0.8, 0, 0))
-		# plt.legend()
-		# plt.xlabel("Time (ns)")
-		# plt.ylabel("Amplitude (mV)")
-		# plt.title("Weighted Curve Fit")
-		# plt.show()
-
 
 #=====================================================================#
 #           Load Doubler Data
@@ -176,7 +28,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_73 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data73 = AP2Analyzer(df_73, -13.52)
+	data73 = AP2Analyzer(df_73, -13.52, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -187,7 +39,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_74 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data74 = AP2Analyzer(df_74, -6)
+	data74 = AP2Analyzer(df_74, -6, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -198,7 +50,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_75 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data75 = AP2Analyzer(df_75, 0)
+	data75 = AP2Analyzer(df_75, 0, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -209,7 +61,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_76 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data76 = AP2Analyzer(df_76, 6)
+	data76 = AP2Analyzer(df_76, 6, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -220,7 +72,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_77 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data77 = AP2Analyzer(df_77, 12)
+	data77 = AP2Analyzer(df_77, 12, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -231,7 +83,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_78 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data78 = AP2Analyzer(df_78, -3)
+	data78 = AP2Analyzer(df_78, -3, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -242,7 +94,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_79 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data79 = AP2Analyzer(df_79, 3)
+	data79 = AP2Analyzer(df_79, 3, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -253,7 +105,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_80 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data80 = AP2Analyzer(df_80, 9)
+	data80 = AP2Analyzer(df_80, 9, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -264,7 +116,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_81 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data81 = AP2Analyzer(df_81, 15)
+	data81 = AP2Analyzer(df_81, 15, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -278,7 +130,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_82 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data82 = AP2Analyzer(df_82, -24.74)
+	data82 = AP2Analyzer(df_82, -24.74, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -289,7 +141,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_83 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data83 = AP2Analyzer(df_83, -18)
+	data83 = AP2Analyzer(df_83, -18, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -300,7 +152,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_84 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data84 = AP2Analyzer(df_84, -12)
+	data84 = AP2Analyzer(df_84, -12, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -311,7 +163,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_85 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data85 = AP2Analyzer(df_85, -6)
+	data85 = AP2Analyzer(df_85, -6, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -322,7 +174,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_86 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data86 = AP2Analyzer(df_86, 0)
+	data86 = AP2Analyzer(df_86, 0, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -333,7 +185,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_87 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data87 = AP2Analyzer(df_87, 6)
+	data87 = AP2Analyzer(df_87, 6, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -344,7 +196,7 @@ try:
 	fn = os.path.join(path, fn)
 	
 	df_88 = pd.read_csv(fn, skiprows=4, encoding='utf-8')
-	data88 = AP2Analyzer(df_88, 12)
+	data88 = AP2Analyzer(df_88, 12, log, trim_time, time_bounds)
 	
 except Exception as e:
 	print(f"Failed to load file {fn} ({e}). Aborting.")
@@ -418,11 +270,16 @@ ax2a = fig2.add_subplot(gs2[0, 0])
 ALP=0.8
 for dat in data_list:
 	c = cmap()
-	ax2a.plot(dat.zcf_time, dat.zcf_freq, linestyle=':', marker='.', label=f"Waveform, P = {dat.power_dBm} dBm", color=c, alpha=ALP, markersize=10)
+	ax2a.plot(dat.zcf_time, dat.zcf_freq, linestyle=':', marker='.', label=f"P = {dat.power_dBm} dBm", color=c, alpha=ALP, markersize=10)
 cmap.reset()
 # ax2a.legend()
 ax2a.grid(True)
 ax2a.set_ylim([0.085, 0.115])
+
+ax2a.set_title("Doubler, 50 ns, IF=100 MHz, Power Sweep")
+ax2a.set_xlabel("Time (ns)")
+ax2a.set_ylabel("Frequency (GHz)")
+ax2a.legend()
 
 #------------------------------
 # Figure 5
--- RP-23 Scripts/SMC-C/Analysis Programs/AP3a_f89_f114_chirp_manual.py
+++ RP-23 Scripts/SMC-C/Analysis Programs/AP3a_f89_f114_chirp_manual.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mplcursors
 
-f_min = np.array([0.09894, 0.09939, 0.09885, 0.09776, 0.09737, 0.09651, 0.09599])
-f_max = np.array([0.10037, 0.10009,  0.10075, 0.10135, 0.10198, 0.1033, 0.10385])
-power_dBm = np.array([-6, -3, 0, 3, 6, 9, 12])
+f_min = np.array([ 0.2915, 0.28697, 0.2895, 0.28953, 0.28693, 0.2817 ])
+f_max = np.array([ 0.3018, 0.30448, 0.3054, 0.30735, 0.32015, 0.32085  ])
+power_dBm = np.array([ -6.62, -3, 0, 6, 9, 12 ])
+
 power_W = 10**(power_dBm/10)*1e-3
 
 f_delta = f_max - f_min
@@ -27,14 +28,12 @@ plt.figure(3)
 plt.plot(power_dBm, f_delta*1e3, color=(0, 0.6, 0), linestyle='--', marker='+')
 plt.grid(True)
 plt.xlabel("Power (dBm)")
-plt.ylim([0, 10])
 plt.ylabel("Frequency Change (MHz)")
 
 plt.figure(4)
 plt.plot(power_W, f_delta*1e3, color=(0, 0.6, 0), linestyle='--', marker='+')
 plt.grid(True)
 plt.xlabel("Power (W)")
-plt.ylim([0, 10])
 plt.ylabel("Frequency Change (MHz)")
 
 mplcursors.cursor(multiple=True)
