6a6c77984e5a2ace75fc8cf97aa9056910c8fc05
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-11-07 10:55:46 -0700

    Updating loss validation sim

--- RP-25 Scripts/TC-C Hallett Sims/SP-1 Loss Validation/run_SP1_loss_validation.py
+++ RP-25 Scripts/TC-C Hallett Sims/SP-1 Loss Validation/run_SP1_loss_validation.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import mplcursors
 
 from hallett.nltsim.core import *
 from hallett.nltsim.analysis import *
@@ -10,16 +11,16 @@ from system_SP1 import *
 parser = argparse.ArgumentParser()
 parser.add_argument("--f0", type=float, default=4e9, help="Stimulus frequency [Hz]")
 parser.add_argument("--V0", type=float, default=1, help="Stimulus amplitude [V]")
-parser.add_argument("--Lmin", type=float, default=-1, help="Min total length [m]")
+parser.add_argument("--Lmin", type=float, default=0.01, help="Min total length [m]")
 parser.add_argument("--Lmax", type=float, default=1, help="Max total length [m]")
 parser.add_argument("--num_sweep", type=int, default=9, help="Number of sweep points")
 parser.add_argument("--dx_ref", type=float, default=None,
 				help="Reference spatial step [m]. If None, use 0.3/600 for FDTD, 0.24/240 for Ladder")
-parser.add_argument("--T", type=float, default=4e-9, help="Total simulation time [s]")
+parser.add_argument("--T", type=float, default=50e-9, help="Total simulation time [s]")
 parser.add_argument("-p", "--parallel", action='store_true', help="Run simulations in parallel")
 parser.add_argument("--tail", type=float, default=1.5e-9, help="Analyze only last 'tail' seconds")
 parser.add_argument("--bw_bins", type=int, default=3, help="Half-width in FFT bins for harmonic integration")
-parser.add_argument("--Vbias", type=float, default="1", help="Applied bias voltage.")
+parser.add_argument("--Vbias", type=float, default=0, help="Applied bias voltage.")
 
 args = parser.parse_args()
 
@@ -33,16 +34,18 @@ def main():
 	
 	total_lenths = np.linspace(args.Lmin, args.Lmax, args.num_sweep)
 	
-	harm_powers = HarmonicPowers(x_parameter="Bias voltage", x_values=bias_vals)
+	harm_powers = HarmonicPowers(x_parameter="Device Length", x_values=total_lenths)
 	
 	t0 = time.time()
 	print(f"Running sequentially.")
+	
+	loss_per_meter = 10
 	
 	# Scan over all lengths
 	for tot_len in total_lenths:
 		
 		# Define system parameters
-		fdtd_params_exp= define_system(tot_len, args.f0, args.V0, args.T, args.dx_ref, False, V_bias=args.Vbias)
+		fdtd_params_exp= define_system(tot_len, args.f0, args.V0, args.T, args.dx_ref, False, V_bias=args.Vbias, LPM=loss_per_meter)
 		
 		fdtd_exp_sim = FiniteDiffSim(fdtd_params_exp) # Create sim
 		fdtd_exp_out = fdtd_exp_sim.run() # run
@@ -60,9 +63,9 @@ def main():
 	
 	plt.figure(1, figsize=(8,5))
 	
-	plt.plot(bias_vals, harm_powers.f0, marker='x', label='Fundamental, Explicit', color=c_fund, linestyle='--')
-	plt.plot(bias_vals, harm_powers.h2, marker='x', label='2nd harmonic, Explicit', color=c_2h, linestyle='--')
-	plt.plot(bias_vals, harm_powers.h3, marker='x', label='3rd harmonic, Explicit', color=c_3h, linestyle='--')
+	plt.plot(total_lenths, harm_powers.f0, marker='x', label='Fundamental, Explicit', color=c_fund, linestyle='--')
+	plt.plot(total_lenths, harm_powers.h2, marker='x', label='2nd harmonic, Explicit', color=c_2h, linestyle='--')
+	plt.plot(total_lenths, harm_powers.h3, marker='x', label='3rd harmonic, Explicit', color=c_3h, linestyle='--')
 	
 	plt.xlabel("Bias Voltage (V)")
 	plt.ylabel("Power at load (dBm)")
@@ -70,7 +73,8 @@ def main():
 	plt.grid(True, alpha=0.3)
 	plt.legend()
 	plt.tight_layout()
-	
+
+	mplcursors.cursor(multiple=True)	
 	
 	plt.show()
 
--- RP-25 Scripts/TC-C Hallett Sims/SP-1 Loss Validation/system_SP1.py
+++ RP-25 Scripts/TC-C Hallett Sims/SP-1 Loss Validation/system_SP1.py
@@ -11,10 +11,14 @@ def loss_to_conductance(C:float, R:float, L:float, loss_dB_per_m):
 	''' Converts loss per meter to conductance (approx).
 	'''
 	
+	Z0 = np.sqrt(L/C)
+	
 	alpha = dB_to_Np(loss_dB_per_m) # Np/m
-	return 4*C*(alpha**2 - R/L)
+	# return 4*C*(alpha**2 - R/L)
+	
+	return (alpha - R/2/Z0)*2/Z0 # Equation from 2.85a in Pozar 4e, rearranged.
 
-def define_system(total_length:float, f0:float, V0:float, t_end:float, dx_ref:float, implicit:bool, V_bias:float=0):
+def define_system(total_length:float, f0:float, V0:float, t_end:float, dx_ref:float, implicit:bool, V_bias:float=0, LPM:float=0):
 	''' Returns parameter objects for the two sim types. Defined in a function so it's easier
 	to recycle the same system into multiple simulations.
 	
@@ -28,13 +32,16 @@ def define_system(total_length:float, f0:float, V0:float, t_end:float, dx_ref:fl
 	dist_L0 = 1e-6
 	dist_C0 = 130e-12
 	
+	G = loss_to_conductance(dist_C0, 0, dist_L0, LPM)
+	# G = 2.62e-2
+	
 	# Define source and load impedances
 	Rs = 50.0
 	RL = 50.0
 	
 	# Define regions
 	regions = [
-		TLINRegion(x0=0.0,     x1=total_length,   L0_per_m=dist_L0, C_per_m=dist_C0, G_per_m=0, alpha=1e-9),
+		TLINRegion(x0=0.0,     x1=total_length,   L0_per_m=dist_L0, C_per_m=dist_C0, G_per_m=G, alpha=1e-9),
 	]
 	
 	# Select a ∆t using the CFL condition
@@ -42,6 +49,9 @@ def define_system(total_length:float, f0:float, V0:float, t_end:float, dx_ref:fl
 	dx = total_length / Nx # Get ∆x
 	Lmin = min(r.L0_per_m for r in regions) # Get max total_length
 	Cmax = max(r.C_per_m for r in regions) # Get max C
+	# print(f"dx = {dx}")
+	# print(f"Lmin = {Lmin}")
+	# print(f"Cmax = {Cmax}")
 	dt = FiniteDiffSim.cfl_dt(dx, Lmin, Cmax, safety=0.85) # Get CFL. Smaller `safety` makes smaller ∆t 
 	
 	# Calculate omega
@@ -57,4 +67,4 @@ def define_system(total_length:float, f0:float, V0:float, t_end:float, dx_ref:fl
 	fdtd_params = FiniteDiffParams(Nx=Nx, total_length=total_length, dt=dt, t_end=t_end, Rs=Rs, RL=RL, Vs_func=Vs, regions=regions, nonlinear_update=update_type)
 
 	# Return param objects
-	return fdtd_param
+	return fdtd_params
